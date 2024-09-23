522022c77f1d76893900d24a8a8afe89bedbb55d
ArelySanchezS13 <arsasaz.96@gmail.com>
2024-09-22 21:20:02 -0600

    tarea para lunes 23/09/24

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("Nuevo Nivel");
+console.error("No puede seguir si no evoluciona a menos que quiera que su pokemon evolucione o que el mismo lo prefiera asi.");
+document.write("Hay que tener paciencia, notos mantienen la inocencia que tenian antes de capturar.");
+alert("No puede seguir si no evoluciona a menos que quiera que su pokemon evolucione o que el mismo lo prefiera asi.");
+document.title="Pokemon";
---
+++ app2.js
@@ -0,0 +1,3 @@
+console.log("Alimentos");
+document.write("Existen varios alimentos pokemón, por eso se tiene que investigar para conocer los gustos y preferencias de cada pokemón a entrenar para que esten satisfechos y felices.");
+document.title="Comidas para el pokemon";
